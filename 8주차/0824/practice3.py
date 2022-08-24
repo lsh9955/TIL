718538aafbdc362782a59c03e6441f1dcfeb3d85
이석훈 <cqe9933@gmail.com>
2022-08-25 00:03:22 +0900

    0824

--- 8주차/0824/practice3.py
+++ 8주차/0824/practice3.py
@@ -15,15 +15,18 @@ def BFS(G,v,n):
             for i in G[t]:
                 if not visited[i]:
                     queue.append(i)
-#
+#방문한 노드를 순서대로 입력할 리스트
 orderList=[]
 inputCase = list(map(int,input()))
+#inputcase개수에 1을 더한 만큼 빈 리스트를 생성(해당 값을 인덱스로 할당할 것이기 때문)
 nodeList = [[] for _ in range(max(inputCase)+1)]
 for k in range(len(inputCase)//2):
+    #각자 연결된 노드 인덱스를 넣음
     nodeList[inputCase[k*2]].append(inputCase[k*2+1])
     nodeList[inputCase[k*2+1]].append(inputCase[k*2])
 BFS(nodeList,1,len(nodeList))
 output = []
+#노드 리스트에서 중복되는 부분을 제하고 출력
 for p in range(len(orderList)-1):
     if orderList[p] ==orderList[p+1]:
         output.append(str(orderList[p]))
